a59f00ef0a3d7ae98b7200749bcf659d812acbfa
naveen <naveen.r.2021.ad@ritchennai.edu.in>
2025-06-22 15:28:27 +0530

    Fix: Run create() on import for Vercel

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import *
 import sqlite3
+
 app = Flask(__name__)
 DATABASE = "database.db"
 
-#CREATE TABLE
+# CREATE TABLE
 def create():
    conn = sqlite3.connect(DATABASE)
    cur = conn.cursor()
@@ -14,22 +15,25 @@ def create():
    conn.commit()
    conn.close()
 
-#INSERT RECORDS TO TABLE
-@app.route("/", methods = ["POST", "GET"])
+# Call create table function at import time
+create()
+
+# INSERT RECORDS TO TABLE
+@app.route("/", methods=["POST", "GET"])
 def index():
    if request.method == "POST":
       name = request.form["name"]
       age = request.form["age"]
       conn = sqlite3.connect(DATABASE)
       cur = conn.cursor()
-      cur.execute("INSERT INTO navy677(name, age) VALUES(?,?)",(name, age))
+      cur.execute("INSERT INTO navy677(name, age) VALUES(?, ?)", (name, age))
       conn.commit()
       conn.close()
       return redirect("/view")
    else:
       return render_template("form.html")
-      
-@app.route("/delete/<int:id>", methods = ["POST", "GET"])
+
+@app.route("/delete/<int:id>", methods=["POST", "GET"])
 def delete(id):
    conn = sqlite3.connect(DATABASE)
    cur = conn.cursor()
@@ -38,16 +42,16 @@ def delete(id):
    conn.close()
    return redirect("/view")
 
-#FETCHING RECORDS FROM THE TABLE
+# FETCHING RECORDS FROM THE TABLE
 @app.route("/view")
 def view():
    conn = sqlite3.connect(DATABASE)
    cur = conn.cursor()
-   cur.execute("select * from navy677")
-   all_rec = cur.fetchall()  #e.g[(1, "naveen", 21), (2, "akask", 30)]
+   cur.execute("SELECT * FROM navy677")
+   all_rec = cur.fetchall()
    conn.close()
-   return render_template("view.html", records = all_rec)
+   return render_template("view.html", records=all_rec)
 
+# Only for local development
 if __name__ == "__main__":
-   create() 
    app.run(debug=True)
